5fe36c5d33e9b2112d3c5d1547826df481aaa3d2
Erik-363 <maciaserik432@gmail.com>
2026-04-22 20:47:45 -0500

    xd

--- nn/model.py
+++
@@ -1,9 +0,0 @@
-class Persona:
-    def __init__(self, nombre, edad):
-        self.nombre = nombre
-        self.edad = edad
-
-    def __str__(self):
-        return f"Nombre: {self.nombre}, Edad: {self.edad}"
-    
-    
